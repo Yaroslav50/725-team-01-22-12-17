b38685d6a5b8b1022d19dfbd8a26e4d8f23b2bac
Evelinach11 <evelinagood2345@gmail.com>
2022-11-11 14:05:09 +0200

    Implemented responsive design for testimonials

--- js/testimonials-card.js
+++ js/testimonials-card.js
@@ -2,10 +2,21 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-    slidesPerView: 3,
+    slidesPerView: 1,
     spaceBetween: 30,
     navigation: {
         nextEl: '.testimonials__cards-prev-btn',
         prevEl: '.testimonials__cards-next-btn',
+    },
+
+    breakpoints:{
+        520: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        750: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        }
     },
 });
