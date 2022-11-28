bc5bde6c3efd454c572c04529272e389e22464a6
VolodymyrB9 <volodyab97@gmail.com>
2022-11-28 14:45:49 +0000

    add js top btn but workig bad

---
+++ js/top-btn.js
@@ -0,0 +1,19 @@
+// History blog by Bilusiak 
+/*
+let topButton = document.getElementById("top-btn__function");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.documentElement.scrollTop = 0; 
+}
+
+*/
