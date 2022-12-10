595f5b6337d203bd6052181358024e5c17e12e49
ZhoYev <zholob0213@gmail.com>
2022-12-10 12:30:20 +0000

    added input validation first

--- js/contact-us.js
+++ js/contact-us.js
@@ -1,11 +1,31 @@
 (() => {
-
+const form = document.querySelector(".contact_us_input");
 let button = document.getElementById("contact_us_btn");
-    button.addEventListener ('click', function() {
-        Swal.fire(
-            'Thank you!',
-            'We will contact you shortly.',
-            'success'
-          )
-    })
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    checkPhoneNumber(this.phone.value);
+  });
+
+  function checkPhoneNumber(phoneNo) {
+    let phoneRE = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
+    if (phoneNo.match(phoneRE)) {
+          button.addEventListener ('click', function() {
+              Swal.fire(
+                  'Thank you!',
+                  'We will contact you shortly.',
+                  'success'
+                )
+          }) ;
+    } else {
+        button.addEventListener ('click', function() {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Please fill out the form!'
+            })
+        });
+    }
+  }
+
 })();    
