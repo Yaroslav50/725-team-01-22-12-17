d9bf0aa84e67972e02fab045e71a293ab0063c3b
ZhoYev <zholob0213@gmail.com>
2022-12-11 10:38:46 +0000

    fixed a bug with double click to confirm the number

--- js/contact-us.js
+++ js/contact-us.js
@@ -10,22 +10,17 @@ form.addEventListener('submit', function(e) {
   function checkPhoneNumber(phoneNo) {
     let phoneRE = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
     if (phoneNo.match(phoneRE)) {
-          button.addEventListener ('click', function() {
               Swal.fire(
                   'Thank you!',
                   'We will contact you shortly.',
                   'success'
                 )
-          }) ;
-    } else {
-        button.addEventListener ('click', function() {
+    } else { 
             Swal.fire({
                 icon: 'error',
                 title: 'Oops...',
                 text: 'Please fill out the form!'
             })
-        });
-    }
+          };
   }
-
 })();    
